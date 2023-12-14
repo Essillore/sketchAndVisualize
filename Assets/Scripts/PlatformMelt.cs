f65a20aef938e662479470091929ed83ba6d8f84
Essillore <riikka.pehkonen@me.com>
2023-12-14 11:15:21 +0200

    Commit 14/12/23_07 Platform fades to opaque before melting

--- Assets/Scripts/PlatformMelt.cs
+++ Assets/Scripts/PlatformMelt.cs
@@ -1,20 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class PlatformMelt : MonoBehaviour
 {
-    public float meltDelay = 5f;
+    private SpriteRenderer spriteRenderer;
+    public float meltDelay = 5.0f; // Duration for the platform to be destroyed
+    public float opacityFadeDuration = 1.9f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, meltDelay);
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeToTransparent(meltDelay - opacityFadeDuration)); // Start fading 1.9 seconds before destruction
+        Destroy(gameObject, meltDelay); // Destroy the object after the meltDelay
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FadeToTransparent(float fadeStartDelay)
     {
-        
+        yield return new WaitForSeconds(fadeStartDelay); // Wait for the fade to start
+
+        float fadeDuration = meltDelay - fadeStartDelay; // Calculate remaining time for fade
+        float currentTime = 0;
+        Color startColor = spriteRenderer.color;
+
+        while (currentTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1.0f, 0.0f, currentTime / fadeDuration);
+            spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null; // Wait until the next frame
+        }
+
+        spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, 0); // Ensure it ends fully transparent
     }
 }
